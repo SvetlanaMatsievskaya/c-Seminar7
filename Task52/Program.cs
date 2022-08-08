6dd126fe86e4b31f1c1cf5b7fd13b669d996691e
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-08-08 17:44:52 +0300

    Tasks completed

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -1,10 +1,10 @@
 ﻿int m = 0;
 int n = 0;
+Console.Write("Введите количество рядов: ");
+int.TryParse(Console.ReadLine(), out m);
+Console.Write("Введите количество столбцов: ");
+int.TryParse(Console.ReadLine(), out n);
 int [,] array = new int[m,n];
-Console.WriteLine("Введите количество рядов: ");
-int.TryParse(Console.WriteLine(), out m);
-Console.WriteLine("Введите количество столбцов: ");
-int.TryParse(Console.WriteLine(), out n);
 for (int i = 0; i < m; i++)
 {
     for (int j = 0; j < n; j++)
@@ -12,15 +12,35 @@ for (int i = 0; i < m; i++)
       array[i,j] = new Random().Next(10);  
     }
 }
-void PrintArray (int[,] array)
+
+
+
+void PrintArray(int[,] array)
 {
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+PrintArray(array);
+void FindAverageColumns (int [,] array)
+{
+    double sum = 0;
+    double average = 0;
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"{array[i,j]} ");
+            sum+=array[j,i];
+            average = Math.Round((sum/n),1);
+            
         }
-        Console.WriteLine();
+        Console.Write($"{average}; ") ;
+        sum = 0;
     }
-
 }
+FindAverageColumns(array);
